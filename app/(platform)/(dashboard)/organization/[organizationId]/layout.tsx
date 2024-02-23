c6eb080c9d2047322678b2e4937a2c4fd5b119cc
David <david.estrelloso.tribugenia@gmail.com>
2024-02-23 10:08:00 +0800

    Mobile sidebar and Sidebar

---
+++ app/(platform)/(dashboard)/organization/[organizationId]/layout.tsx
@@ -0,0 +1,12 @@
+import { OrgControl } from "./_components/Org-Control";
+
+const OrganizationIdLayout = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <>
+      <OrgControl />
+      {children}
+    </>
+  );
+};
+
+export default OrganizationIdLayout;
